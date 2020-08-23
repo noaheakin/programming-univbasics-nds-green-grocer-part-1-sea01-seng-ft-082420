0788cb8c974d7baf7ce1590c207138e86483735d
Noah Eakin <noaheakin@gmail.com>
2020-08-23 01:10:53 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,13 +1,12 @@
 require 'pry'
-def find_item_by_name_in_collection(name, collection)
-  collection.each do |items|
-    if items[:item] == name
-      return items
+def find_item_by_name_in_collection (item, shelf)
+  shelf.each do |groceries|
+    if groceries[:item] == item
+      return groceries
     end
   end
   return nil
 end
-
 
 def consolidate_cart (cart)
   new_cart = []
